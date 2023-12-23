d9d6910375f4e4e0d9f82faf815dfdd9a47b778e
Devansh Tiwari <65783463+devloves@users.noreply.github.com>
2023-12-23 20:03:31 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import { Rubik } from 'next/font/google';
-import { Metadata } from 'next';
 
 const rubik = Rubik({
   subsets: ['latin'],
@@ -8,12 +7,6 @@ const rubik = Rubik({
 });
 
 function MyApp({ Component, pageProps }) {
-  // Move the metadata declaration inside the MyApp function
-  const metadata: Metadata = {
-    title: "Devansh Tiwari's Portfolio",
-    description: 'Front-End Developer & Designer from India.',
-  };
-
   return (
     <main className={`${rubik.variable} font-sans`}>
       <Component {...pageProps} />
