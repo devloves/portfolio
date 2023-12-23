336f97e2ca9e5a8f107aaf4362c0669eabebd0b7
Devansh Tiwari <65783463+devloves@users.noreply.github.com>
2023-12-23 20:02:47 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,24 @@
-import '../styles/globals.css'
-import { Rubik } from 'next/font/google'
-import { Metadata } from 'next'
+import '../styles/globals.css';
+import { Rubik } from 'next/font/google';
+import { Metadata } from 'next';
 
 const rubik = Rubik({
   subsets: ['latin'],
   variable: '--font-rubik',
-})
- 
-export default function MyApp({ Component, pageProps }) {
+});
+
+function MyApp({ Component, pageProps }) {
+  // Move the metadata declaration inside the MyApp function
   const metadata: Metadata = {
     title: "Devansh Tiwari's Portfolio",
     description: 'Front-End Developer & Designer from India.',
   };
-  
+
   return (
     <main className={`${rubik.variable} font-sans`}>
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
+
+export default MyApp;
